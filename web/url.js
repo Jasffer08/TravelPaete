289dbb8598e4b27fa3e107381419ee92a5dfd556
Jasffer08 <136311842+Jasffer08@users.noreply.github.com>
2023-06-12 16:28:37 +0800

    Delete web directory

--- web/url.js
+++
@@ -1,7 +0,0 @@
-document.write("My name<h1> is Jasffer R. Catapat</h1><br>")
-document.write("I am 20 years old.<br>" )
-document.write("I live in the small town in Laguna, Paete.<br>" )
-document.write("My hobby is to play badminton.<br>" )
-document.write("I am currently studying on SLSU. <br>" )
-
-    
